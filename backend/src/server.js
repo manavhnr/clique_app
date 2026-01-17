ce9a2f00a186108a711b50f45cbd0437e06b92c8
Manavh <manavh160206@gmail.com>
2026-01-17 10:33:30 +0530

    user routes add

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,38 @@
 import express from 'express';
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/expess"
+
+import userRoutes from './routes/user.route.js'; 
+
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
 
 const app = express();
 
-connectDB();
+app.use(cors())
+app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
-app.listen(ENV.PORT, () => 
-  console.log('Server is running on port:', ENV.PORT)
-); 
+
+app.use("/api/users",userRoutes)
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+
+startServer();
